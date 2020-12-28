7d7ead70bf160de40023ec598b913518437da87e
Sheila Leveille <sbleveille@gmail.com>
2020-12-28 13:47:23 -0500

    FEAT: location calendar and media queries

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -6,4 +6,8 @@ module CalendarHelper
     def calendar_ajax_next_link
         ->(param, date_range) { link_to raw("&raquo;"), {param => date_range.last + 1.day}, remote: :true}
     end
+
+    def date_today
+        Date.today.to_s.to_sym
+    end
 end
