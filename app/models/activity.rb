ce73d8c75c8d46ad3652ee02da99fe6edf3bc956
Sheila Leveille <sbleveille@gmail.com>
2021-01-08 17:10:06 -0500

    FEAT: activity cards for index and show

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,6 @@ class Activity < ApplicationRecord
     
     has_many :activity_lists
     has_many :events, through: :activity_lists
-    has_many :locations, through: :events
 
     has_one_attached :photo
 
@@ -15,7 +14,12 @@ class Activity < ApplicationRecord
         left_joins(:activity_lists).group(:id).order('COUNT(activity_lists.id) DESC').limit(12)
     end
 
+    def upcoming_events
+        events.where('start_date >= ?', Date.today).order(start_date: :desc).limit(3)
+    end
+
     def activity_locations
+        Location.joins(events: :activity_lists).where(activity_lists: { activity_id: self.id })
         # Top ten activites by number of events
         #Activity.all.map{ |x| [Event.where(activity_id: x.id).count, x] }.sort.reverse!
     end
