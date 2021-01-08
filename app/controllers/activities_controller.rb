35c8b9ff79899802e97d02dfca58a1069ebdd507
Sheila Leveille <sbleveille@gmail.com>
2021-01-08 13:08:35 -0500

    FEAT: activities show & index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,11 @@
 class ActivitiesController < ApplicationController
+
+    def index
+        @activities = policy_scope(Activity)
+    end
+
+    def show
+        @activity = Activity.find(params[:id])
+        authorize @activity
+    end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,10 +1,22 @@
 class Activity < ApplicationRecord
+    default_scope { order('title ASC') }
+    
     has_many :activity_lists
     has_many :events, through: :activity_lists
+    has_many :locations, through: :events
 
     has_one_attached :photo
 
     def self.most_popular_activities
+        left_joins(:activity_lists).group(:id).order('COUNT(activity_lists.id) DESC')
+    end
+
+    def self.top_twelve_activities
         left_joins(:activity_lists).group(:id).order('COUNT(activity_lists.id) DESC').limit(12)
+    end
+
+    def activity_locations
+        # Top ten activites by number of events
+        #Activity.all.map{ |x| [Event.where(activity_id: x.id).count, x] }.sort.reverse!
     end
 end
