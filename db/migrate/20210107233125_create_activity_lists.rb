6b434b267724aa2ca3705df0cc51884e6d0ad911
Sheila Leveille <sbleveille@gmail.com>
2021-01-08 17:29:18 -0500

    FIX: activity list migration fix

--- db/migrate/20210107233125_create_activity_lists.rb
+++ db/migrate/20210107233125_create_activity_lists.rb
@@ -1,4 +1,4 @@
-class CreateActivityEvents < ActiveRecord::Migration[6.0]
+class CreateActivityLists < ActiveRecord::Migration[6.0]
   def change
     create_table :activity_lists do |t|
       t.references :activity, null: false, foreign_key: true
