6fd9400ab16098669ee7131930a242b9a08b258c
Sheila Leveille <sbleveille@gmail.com>
2021-01-26 11:21:33 -0500

    FEAT: enable skip authentication for index and show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :index, :show ]
 
     def index
         @events = policy_scope(Event)
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :index, :show ]
     before_action :set_trip, only: [ :show, :update, :edit, :destroy ]
     
     def index
