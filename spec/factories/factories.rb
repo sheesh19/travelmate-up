09ec67cd5143cf7ac01788d40bb80c0fd3a4902b
Sheila Leveille <sbleveille@gmail.com>
2021-06-02 10:48:27 -0500

    FEAT: Add trip specs

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,18 +1,20 @@
 FactoryBot.define do
 
+    factory(:activity) do
+        title { 'Snorkeling' }
+        id { 1 }
+    end
+
+    factory(:trip) do
+        title { 'Test Trip' }
+        start_date { Date.today + 30.days }
+        end_date { Date.today + 40.days }
+        user
+    end
+
     factory(:user) do
         email { 'user@user.com' }
         password { 'password' }
     end
     
-    factory(:trip) do
-        title { 'Test Trip' }
-        start_date { Date.now + 30.days }
-        end_date { Date.now + 40.days }
-        user
-    end
-
-    factory(:activity) do
-        title { 'Test Activity' }
-    end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "trip" do
   it "requires a trip to belong to a user" do
     trip = Trip.new
-
+    
     expect(trip).not_to be_valid
   end
 end
